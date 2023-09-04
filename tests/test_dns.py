a952d315eb2f05f7aeedfd22d8508d3354d906e7
Criss Wang <djj87018507@gmail.com>
2023-09-04 04:13:35 -0400

    remove dns

--- tests/test_dns.py
+++ tests/test_dns.py
@@ -1,16 +1,8 @@
 from dns import dns
 from .base_test import BaseTest
 
-import os
-import logging
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s %(levelname)s: %(message)s",
-)
-path = os.path.abspath(dns.__file__)
 
 class TestDNS(BaseTest):
     def test_get_flags(self):
-        logging.info(path)
         mock_data = int(1).to_bytes(1, byteorder='big') * 2
         self.assertEqual(dns.get_flags(mock_data), b'\x84\x00')
